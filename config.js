40c868af89e4b79e58b0e93d3e704c9611f085e7
vsdragoncityemirhan0 <101357414+vsdragoncityemirhan0@users.noreply.github.com>
2022-03-10 21:25:39 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Token: "OTAzMjc3MzU2NTQ5MDM4MTYy.YinccQ.XtPBP3e2crkd8_lUBdUIqZj5kvY", 
+  Prefix: "!",
+  BotOwner: "811620631703584772", //kendi hesap idnizi yazın
+  OwoBotID: "408785106942164992", // Varsayılan Değiştirmeyin!
+  UserID: [811620631703584772], // Bu kısıma arkadaşlarınızın ID'sini yazabilirsiniz bot spam'a düşmesin diye yapıldı. arada bir ıdsi girilen kişilere etiket atar.
+  GuildID: "949680047311167568" //botun mesaj atacağı sunucu id yaz
+};
